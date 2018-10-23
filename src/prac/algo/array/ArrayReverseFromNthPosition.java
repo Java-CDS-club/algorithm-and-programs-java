0864464bde88020fe6bb3f9e72353511c14ce215
suhailmansoori <suhailmansoori@10INLPC0VSLYV.reg1.1bank.dbs.com>
2018-10-23 16:40:17 +0530

    Commit: Array Reverse from nth position.

---
+++ src/prac/algo/array/ArrayReverseFromNthPosition.java
@@ -0,0 +1,33 @@
+package prac.algo.array;
+
+import java.util.Arrays;
+
+public class ArrayReverseFromNthPosition {
+
+	/*
+	 * Algo to Reverse an Array from given nth positions.
+	 * 
+	 */
+
+	private static void rotate(char[] input, int position) {
+		System.out.println("Input:>	" + Arrays.toString(input));
+		if ((input != null && input.length == 0) || input.length < position) {
+			System.out.println("No Input or Invalid Input.");
+			return;
+		}
+		int length = input.length;
+		for (int i = 0; i < (length - position) / 2; i++) {
+			char temp = input[i + position];
+			input[i + position] = input[length - i - 1];
+			input[length - i - 1] = temp;
+		}
+		
+		System.out.println("Output:>" +  Arrays.toString(input));
+	}
+
+	public static void main(String[] args) {
+		String input = "1234567";
+		rotate(input.toCharArray(), 1);
+	}
+
+}
