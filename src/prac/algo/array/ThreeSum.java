355d4971c854668ec8ef920f6f3e73f07eb8a5fa
suhailmansoori <suhailmansoori@10INLPC0VSLYV.reg1.1bank.dbs.com>
2018-10-24 18:08:38 +0530

    3Sum Solution.

--- src/prac/algo/array/ThreeSum.java
+++ src/prac/algo/array/ThreeSum.java
@@ -1,12 +1,13 @@
 package prac.algo.array;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ThreeSum {
 
 	/**
-	 * Given an array with numbers. find the triplets where sum=0.
-	 * O(N^2)
+	 * Given an array with numbers. find the triplets where sum=0. O(N^2)
 	 */
 
 	private static void find(int[] input) {
@@ -26,24 +27,31 @@ public class ThreeSum {
 			return;
 		}
 
+		Arrays.sort(input);
+
+		Map<String, Integer> trips = new HashMap<>();
 		for (int i = 0; i < input.length - 1; i++) {
-			int sum[] = new int[3];
+			int []sum = new int[3];
 			sum[0] = input[i];
 			sum[1] = input[i + 1];
 
 			for (int k = 0; k < input.length; k++) {
 				if (k != i && k != (i + 1) && k != i - 1) {
 					sum[2] = input[k];
-
-					System.out.println("Triplets: " + Arrays.toString(sum) + "" + "  Sum: "
-							+ Arrays.stream(sum).reduce(0, Integer::sum));
+					trips.put(sum[0]+","+sum[1]+","+sum[2],  Arrays.stream(sum).reduce(0, Integer::sum));
 				}
 			}
 		}
+		
+		for (String key : trips.keySet()) {
+			if(trips.get(key)==0)
+				System.out.println("Triplet: "+key+", Sum: "+trips.get(key));
+		}
+
 	}
 
 	public static void main(String[] args) {
-		int[] input = { -1, -1, 0, 2, 2, 3 };
+		int[] input = { -1, 0, 1, 2, -1, -4 };
 		find(input);
 	}
 
