33265228d02f2d2cd17d40862026139267999658
Suhail <Suhail@Suhail-PC>
2021-01-10 00:24:54 +0530

    commmit.

--- src/prac/functional/FunctionalProgramming.java
+++ src/prac/functional/FunctionalProgramming.java
@@ -1,45 +1,58 @@
 package prac.functional;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.IntConsumer;
 import java.util.stream.IntStream;
 
 public class FunctionalProgramming {
-	
+
 	/*
 	 * public void test() { int count=0; new Thread(()->{ System.out.println("Hi");
 	 * }); }
 	 */
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		List<Person> persons = new ArrayList<>();
-		persons.add(new Person("Suhail", "Mansoori"));
-		persons.add(new Person("David", "Romania"));
-		persons.add(new Person("Backham", "Procos"));
-		persons.add(new Person("Winter", "Singer"));
-		persons.add(new Person("Abraham", "Linkon"));
-		persons.add(new Person("Cotlin", "Adrew"));
+		persons.add(new Person("Suhail", "Mansoori", 18));
+		persons.add(new Person("David", "Romania", 15));
+		persons.add(new Person("Backham", "Procos", 35));
+		persons.add(new Person("Winter", "Singer", 40));
+		persons.add(new Person("Abraham", "Linkon", 33));
+		persons.add(new Person("Cotlin", "Adrew", 55));
 
-		persons.stream().sorted((a, b) -> a.getLastName().compareTo(b.getLastName())).
-		forEach(p->System.out.println(p.getLastName()));
+		persons.stream().sorted((a, b) -> a.getLastName().compareTo(b.getLastName()))
+				.forEach(p -> System.out.println(p.getLastName()));
+
+		IntStream.range(1, 10).forEach(i -> System.out.println(i));
+
+		System.out.println(IntStream.range(1, 8).sum());
 		
-		
-		IntStream.range(1, 10).forEach(i->System.out.println(i));
-		
+		System.out.println(persons.stream().map(Person::getAge).reduce(0, (a,b)->a+b));
+
+		// readFromFile();
 
 	}
+
+	/*
+	 * private static void readFromFile() throws IOException { List<String> lines =
+	 * Files.lines(Paths.get("data.txt")).filter(line -> line.startsWith("B"))
+	 * .collect(Collectors.toList()); lines.stream().forEach(line ->
+	 * System.out.println(line)); }
+	 */
 
 }
 
 class Person {
 	String firstName;
 	String lastName;
+	int age;
 
-	public Person(String firstName, String lastName) {
+	public Person(String firstName, String lastName, int age) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.age = age;
 	}
 
 	public String getFirstName() {
@@ -50,11 +63,6 @@ class Person {
 		this.firstName = firstName;
 	}
 
-	@Override
-	public String toString() {
-		return "Person [firstName=" + firstName + ", lastName=" + lastName + "]";
-	}
-
 	public String getLastName() {
 		return lastName;
 	}
@@ -63,4 +71,16 @@ class Person {
 		this.lastName = lastName;
 	}
 
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + "]";
+	}
 }
